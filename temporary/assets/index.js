05d77227feeccf50662b2d1498ba07964babe120
remybms <112947392+remybms@users.noreply.github.com>
2025-06-23 15:01:30 +0200

    delete useless

--- temporary/assets/index.js
+++
@@ -1,8 +0,0 @@
-import prompt from "./prompt.js"
-
-async function greet(){
-    const name = await prompt("Saisissez un nom");
-    console.log(`Hello, ${name}!`);
-}
-greet();
-
--- temporary/env/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Doing stuff...");
-setTimeout(() => {
-    console.log("Hello, World!");
-    process.exit(0);
-}, 1000);
