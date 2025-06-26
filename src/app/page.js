f53cd93f99a9fc2fd0711656d1bc908a665972b2
letienne <etienne.lemee@ynov.com>
2025-06-26 14:25:50 +0200

    feat: added btn that sends to create archive page

--- src/app/page.js
+++ src/app/page.js
@@ -1,1 +1,16 @@
-export default function Home() {}
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  
+  return (
+    <main>   
+      <h1>Welcome to CodePrez</h1> 
+      <button
+      onClick={() => router.push("/create-archive")}>
+      Create an archive
+      </button>
+    </main>
+  );
+}
