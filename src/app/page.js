dcabcd4635fa264907ec8c761e33ac7db4cc830e
remybms <112947392+remybms@users.noreply.github.com>
2025-06-26 15:03:50 +0200

    display markdown on page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import Image from "next/image";
+"use client"
+
 import styles from "./page.module.css";
+import MarkdownIt from "markdown-it";
+import React from "react";
 
 export default function Home() {
+
+  const md = new MarkdownIt()
+
+  React.useEffect(() => {
+    const markdown = document.querySelector('main')
+    markdown.innerHTML = "Aucune présentation n'est ouverte"
+    window.api.onOpenFolder((data) => {
+      console.log(data)
+      markdown.innerHTML = md.render(data)
+    })
+  }, [])
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
       </main>
     </div>
   );
