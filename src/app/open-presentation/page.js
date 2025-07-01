dd8b41fb9c1d99806f5942bce80af06d5f5acfee
letienne <etienne.lemee@ynov.com>
2025-07-01 15:39:08 +0200

    feat: frontend modifications

--- src/app/open-presentation/page.js
+++ src/app/open-presentation/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [content, setContent] = useState("");
   const [slides, setSlides] = useState([]);
   const [current, setCurrent] = useState(0);
-
+  
   const md = React.useMemo(() => new MarkdownIt({
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
@@ -20,7 +20,7 @@ export default function Home() {
       return '';
     }
   }), []);
-
+  
   React.useEffect(() => {
     (async () => {
       const listResult = await window.api.requestSlidesList();
@@ -41,7 +41,7 @@ export default function Home() {
       }
     })();
   }, []);
-
+  
   const goToSlide = async (idx) => {
     if (slides.length === 0) return;
     const contentResult = await window.api.requestSlideContent(slides[idx]);
@@ -54,21 +54,19 @@ export default function Home() {
       }, 0);
     }
   };
-
+  
   return (
     <div className={styles.wholePage}>
-      <a href="/" className={styles.logoLink}>
-        <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px"}} />
-      </a>
-      
-      <div>
-        <h2>Presentation mode</h2>
-        <div style={{marginBottom: '1rem'}}>
-          <button onClick={() => goToSlide(Math.max(0, current - 1))} disabled={current === 0 || slides.length === 0}>Previous</button>
-          <button onClick={() => goToSlide(Math.min(slides.length - 1, current + 1))} disabled={current === slides.length - 1 || slides.length === 0}>Next</button>
-        </div>
-        <div id="presentation" dangerouslySetInnerHTML={{ __html: content ? md.render(content) : "No opened presentations" }} />
-      </div>
+    <a href="/" className={styles.logoLink}>
+    <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px"}} />
+    </a>
+    
+    <div id="presentation" className={styles.presentation} dangerouslySetInnerHTML={{ __html: content ? md.render(content) : "No opened presentations" }} />
+    
+    <div className={styles.presentationContainer} style={{marginBottom: '1rem'}}>
+    <button onClick={() => goToSlide(Math.max(0, current - 1))} disabled={current === 0 || slides.length === 0}>&lt;</button>
+    <button onClick={() => goToSlide(Math.min(slides.length - 1, current + 1))} disabled={current === slides.length - 1 || slides.length === 0}>&gt;</button>
+    </div>
     </div>
   );
 }
