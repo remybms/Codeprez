7b5ada7d85647e1969795343fe0b2b5028ef0895
remybms <112947392+remybms@users.noreply.github.com>
2025-07-01 18:54:44 +0200

    delete useless code and add css from the .codeprez

--- src/app/open-presentation/page.js
+++ src/app/open-presentation/page.js
@@ -4,27 +4,23 @@ import MarkdownIt from "markdown-it";
 import hljs from 'highlight.js';
 import { useState } from "react";
 import styles from "./page.module.css";
+import { convertCode } from "../../../scripts/convertCode";
 
 export default function Home() {
   const [content, setContent] = useState("");
   const [slides, setSlides] = useState([]);
   const [current, setCurrent] = useState(0);
-  
-  const md = React.useMemo(() => new MarkdownIt({
-    highlight: function (str, lang) {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(str, { language: lang }).value;
-        } catch (__) {}
-      }
-      return '';
-    }
-  }), []);
-  
+
+  const md = new MarkdownIt()
+
   React.useEffect(() => {
     (async () => {
       const listResult = await window.api.requestSlidesList();
       if (listResult.success && listResult.files.length > 0) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '/style.css';
+        document.head.appendChild(link);
         setSlides(listResult.files);
         setCurrent(0);
         const contentResult = await window.api.requestSlideContent(listResult.files[0]);
@@ -41,32 +37,34 @@ export default function Home() {
       }
     })();
   }, []);
-  
+
   const goToSlide = async (idx) => {
     if (slides.length === 0) return;
     const contentResult = await window.api.requestSlideContent(slides[idx]);
     if (contentResult.success) {
       setContent(contentResult.content);
+      hljs.highlightAll()
       setCurrent(idx);
       setTimeout(() => {
         hljs.configure({ classPrefix: "" });
         hljs.highlightAll();
-      }, 0);
+        convertCode(hljs)
+      }, 1);
     }
   };
-  
+
   return (
     <div className={styles.wholePage}>
-    <a href="/" className={styles.logoLink}>
-    <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px"}} />
-    </a>
-    
-    <div id="presentation" className={styles.presentation} dangerouslySetInnerHTML={{ __html: content ? md.render(content) : "No opened presentations" }} />
-    
-    <div className={styles.presentationContainer} style={{marginBottom: '1rem'}}>
-    <button onClick={() => goToSlide(Math.max(0, current - 1))} disabled={current === 0 || slides.length === 0}>&lt;</button>
-    <button onClick={() => goToSlide(Math.min(slides.length - 1, current + 1))} disabled={current === slides.length - 1 || slides.length === 0}>&gt;</button>
-    </div>
+      <a href="/" className={styles.logoLink}>
+        <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px" }} />
+      </a>
+
+      <section id="presentation" className={styles.presentation} dangerouslySetInnerHTML={{ __html: content ? md.render(content) : "No opened presentations" }} />
+
+      <div className={styles.presentationContainer} style={{ marginBottom: '1rem' }}>
+        <button onClick={() => goToSlide(Math.max(0, current - 1))} disabled={current === 0 || slides.length === 0}>&lt;</button>
+        <button onClick={() => goToSlide(Math.min(slides.length - 1, current + 1))} disabled={current === slides.length - 1 || slides.length === 0}>&gt;</button>
+      </div>
     </div>
   );
 }
