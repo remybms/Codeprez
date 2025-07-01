6b6097808d17bddd172af1c7a4fd01cbdd00e99d
letienne <etienne.lemee@ynov.com>
2025-07-01 13:32:07 +0200

    feat: added logo and style

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,25 @@
 "use client";
 import { useRouter } from "next/navigation"; 
+// import styles from "./globals.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   const router = useRouter();
   
   return (
-    <main>   
-      <h1>Welcome to CodePrez</h1> 
-      <br/>
-      <img src="/logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "200px"}} />
-      <br/>
-      <br/>
-      {/* <button
+    <div className={styles.wholePage}>   
+    <a href="/" className={styles.logoLink}>
+      <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px"}} />
+    </a>
+    <h1>Welcome to CodePrez</h1> 
+    <br/>
+    <br/>
+    <br/>
+    {/* <button
       onClick={() => router.push("/create-archive")}>
       Create an archive
       </button>     */}
-    </main>
-  );
-}
+      </div>
+    );
+  }
+  
