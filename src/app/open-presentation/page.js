9b1d8bbd6fbfdb1b5e44bfe3124736aad3d09a92
letienne <etienne.lemee@ynov.com>
2025-07-01 13:32:59 +0200

    feat: new presentation page

---
+++ src/app/open-presentation/page.js
@@ -0,0 +1,23 @@
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  
+  return (
+    <div className={styles.wholePage}>
+      <a href="/" className={styles.logoLink}>
+        <img src="./logo/codeprez-full-logo.png" alt="CodePrez Logo" style={{ width: "250px"}} />
+      </a>
+      
+      <div >
+        <h1>Presentation mode</h1>
+        
+
+        <div >
+          {/* <button onClick={handleSubmit}>Create .codeprez</button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
